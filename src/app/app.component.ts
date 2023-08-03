3652a5e10c7f28100e67b4d8a86ff0c9678887f6
AbhiMophare <77708148+AbhiMophare@users.noreply.github.com>
2023-08-03 17:14:34 +0530

    side-nav and chat component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ChatComponent } from './chat/chat.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @Component({
   standalone: true,
   selector: 'whatsapp',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [CommonModule, SideNavComponent, ChatComponent],
 })
 export class AppComponent implements OnInit {
   constructor() {}
